cb9f8b7b36b4432b3a24051755b98b6b24077ecd
Aaron Segura <110490003+ArontiDumpti@users.noreply.github.com>
2023-12-17 21:59:53 -0500

    Add files via upload

---
+++ inc/head.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FARMACIA EBENEZER</title>
+    <link rel="stylesheet" href="./css/bulma.min.css">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <!-- Agregar el icono a la pestaña del navegador -->
+    <link rel="icon" type="image/png" href="./img/icono.jpg">
